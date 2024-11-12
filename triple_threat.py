1708363632d17c5d4dff8a1d96112023f30275cb
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-11-12 02:32:19 +0000

    Create triple_threat.py with starter code.

---
+++ triple_threat.py
@@ -0,0 +1,21 @@
+"""
+Triple threat simulation game for AP Computer Science Principles.
+Monarch High School - Boulder Valley School District
+Name - Month Year
+"""
+
+import random
+
+def main() -> None:
+    pass # remove
+    # set variables for cost to play and base prize
+
+    # roll three dice
+
+    # check if they are equal   
+    # if they are, calculate the prize
+
+    # output results
+
+if __name__ == "__main__":
+    main()
